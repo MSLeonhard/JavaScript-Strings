96f284d0a4b9718f4805bc90e34027eaeee84959
Michael Leonhard <77648651+MSLeonhard@users.noreply.github.com>
2021-02-02 15:45:14 -0500

    completed step 5

--- basics-1.js
+++ basics-1.js
@@ -1,3 +1,8 @@
 console.log("Let's do this!")
-var name = prompt("Enter some text")
+let name = prompt("Enter some text")
 alert(`The number of characters you entered is ${name.length}`)
+
+
+const check = name.indexOf("l")
+
+console.log(check)
